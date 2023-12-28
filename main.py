f5b97d3e2abbc41bf23c6972d7167cee3fa6a690
Dmitry Vergelsky <graf.werger@gmail.com>
2023-12-28 23:44:12 +0700

    1

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import os
+import pathlib
+import textwrap
 
-print(os.environ.get('GAPI_KEY'))
+import google.generativeai as genai
+
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+genai.configure(api_key=os.environ.get('GAPI_KEY'))
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
